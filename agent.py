abb0e0237e09086f3ca32153008fab393008e97b
이영기 <at.oz.wizard@gmail.com>
2026-01-08 10:43:38 +0900

    fix: 디버깅 코드 추가

--- agent.py
+++ agent.py
@@ -3,6 +3,7 @@ from tools import get_weather
 
 class LlmAgent:
   def handle(self, user, message):
+    print(f"디버깅 중...{user}  {message}")
     if "날씨" in message:
       weather = get_weather("서울")
       return f"[LLM]{user}님, 서울의 날씨는 '{weather}' 입니다."
